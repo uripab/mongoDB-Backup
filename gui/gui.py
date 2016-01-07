c663e9c95c0b40eb3c11474e1de4cc705e738a29
Uriel Pavlov <uripab@egmail.com>
2016-01-07 07:58:14 -0800

    change path

--- gui/gui.py
+++ gui/gui.py
@@ -3,16 +3,17 @@ import json
 import bottle
 from bottle import route, run, view,template
 import sys
-sys.path.append("/home/uripab/mongo_backup")
+sys.path.append("/home/uripab/mycode/mongoDB-Backup/db_code/")
 import student_main
 
+my_student =student_main.student()
 student_column =["student_id","first_name","last_name","course"]
 
 @route('/')
 @route('/Standalone')
 @view('student')
 def standalone():
-    data=student_main.get_student()
+    data=my_student.get_student()
     return dict(title='students',db_type='Standalone',data=data,student_column=student_column)
 
 
@@ -21,8 +22,8 @@ def standalone():
 def replicaset():
     primary_port = "27017"
     secendary_port = "27018"
-    primary_data=student_main.get_student_from_replicaset(primary_port)
-    secendary_data=student_main.get_student_from_replicaset(secendary_port)
+    primary_data=my_student.get_student_from_replicaset(primary_port)
+    secendary_data=my_student.get_student_from_replicaset(secendary_port)
     return dict(title='Replica-set',db_type='Replica-set',data=primary_data,secendary_data =secendary_data,
                 student_column=student_column,primary_port=primary_port,secendary_port=secendary_port)
 
@@ -33,8 +34,8 @@ def replicaset():
 def sharding():
     primary_port = "37017"
     secendary_port = "37018"
-    primary_data=student_main.get_student_from_replicaset(primary_port)
-    secendary_data=student_main.get_student_from_replicaset(secendary_port)
+    primary_data=my_student.get_student_from_replicaset(primary_port)
+    secendary_data=my_student.get_student_from_replicaset(secendary_port)
     return dict(title='Shard-1',db_type='Shardingt',data=primary_data,secendary_data =secendary_data,
                 student_column=student_column,primary_port=primary_port,secendary_port=secendary_port)
 
@@ -44,8 +45,8 @@ def sharding():
 def sharding():
     primary_port = "47017"
     secendary_port = "47018"
-    primary_data=student_main.get_student_from_replicaset(primary_port)
-    secendary_data=student_main.get_student_from_replicaset(secendary_port)
+    primary_data=my_student.get_student_from_replicaset(primary_port)
+    secendary_data=my_student.get_student_from_replicaset(secendary_port)
     return dict(title='Shard-2',db_type='Shardingt',data=primary_data,secendary_data =secendary_data,
                 student_column=student_column,primary_port=primary_port,secendary_port=secendary_port)
 
@@ -54,11 +55,10 @@ def sharding():
 def sharding():
     primary_port = "57017"
     secendary_port = "57018"
-    primary_data=student_main.get_student_from_replicaset(primary_port)
-    secendary_data=student_main.get_student_from_replicaset(secendary_port)
+    primary_data=my_student.get_student_from_replicaset(primary_port)
+    secendary_data=my_student.get_student_from_replicaset(secendary_port)
     return dict(title='Shard-3',db_type='Shardingt',data=primary_data,secendary_data =secendary_data,
                 student_column=student_column,primary_port=primary_port,secendary_port=secendary_port)
 
 run(host='0.0.0.0', port=8889, debug=True)
-
-
+#run(host='localhost', port=8889, debug=True)
